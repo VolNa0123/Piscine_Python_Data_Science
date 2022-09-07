6db713b95fba3cde42d576b0711f35266b1fbc17
Melnitz Cherrie <mcherrie@at-i5.msk.21-school.ru>
2022-09-07 18:31:03 +0300

    ex05 ok

---
+++ Day_02/ex05/config.py
@@ -0,0 +1,8 @@
+FILEPATH = 'data.csv'
+NUM_OF_STEPS = 3
+REPORT_FILE = 'report'
+
+REPORT = """Report
+We have made {} observations from tossing a coin: {} of them were tails and {} of
+them were heads. The probabilities are {}% and {}%, respectively. Our
+forecast is that in the next {} observations we will have: {} tail and {} heads."""
